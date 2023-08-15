fd7c6cca4bc3f23b2969cb02de6f0cd586b8e714
vaikin9999 <142353250+vaikin9999@users.noreply.github.com>
2023-08-15 15:33:03 -0700

    pepe.php

---
+++ pepe.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola mundo de pepe</title>
+</head>
+<body>
+   <p> Hola mundo 2 c:</p> 
+</body>
+</html>
